c3532fa9d44dab630f608c121df90795353e92eb
Oliver <oliv5023@gmail.com>
2023-12-05 09:52:46 +0100

    day3part1 result is off by 283

--- Solutions/days/Day3.cs
+++ Solutions/days/Day3.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 namespace Days {
     public class Day3
@@ -25,20 +24,19 @@ namespace Days {
         private static bool CheckIfNumberIsPartNumber(int begin, int end, int i, string[] lines)
         {
             string line = lines[i];
-            if(begin != 0 && !line[begin-1].Equals('.')) {
+            if(begin > 0 && !line[begin-1].Equals('.')) {
                 return true;
             }
-            if(end != line.Length-1 && !line[line.Length-1].Equals('.')) {
+            if(end < line.Length-1 && !line[end+1].Equals('.')) {
                 return true;
             }
-            for(int j = begin-1; j < end+1; j++) {
-                if(j < 0 | j > line.Length-1) {
+            for(int j = begin-1; j < end+2; j++) {
+                if(j < 0 || j > line.Length-1) {
                     continue;
                 }
-                if(i != 0 && !lines[i-1][j].Equals('.') || i != lines.Length-1 && !lines[i+1][j].Equals('.')) {
+                if(i > 0 && !lines[i-1][j].Equals('.') || i < lines.Length-1 && !lines[i+1][j].Equals('.')) {
                     return true;
                 }
-                
             }
             return false;
         }
@@ -50,6 +48,16 @@ namespace Days {
         public static void SolveDay3()
         {
             string[] lines = File.ReadAllLines("../inputs/input3.txt");
+            string[] input = {"467..114..",
+                        "...*......",
+                        "..35..633.",
+                        "......#...",
+                        "617*......",
+                        ".....+.58.",
+                        "..592.....",
+                        "......755.",
+                        "...$.*....",
+                        ".664.598.."};
             Console.WriteLine(SolvePartOne(lines));
             Console.WriteLine("Part one: ");
             Console.WriteLine("Part two: ");
