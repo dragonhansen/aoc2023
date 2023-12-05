60b6b9d1ae2f4f09d516c5cc6a4d2302df115ca7
Oliver <oliv5023@gmail.com>
2023-12-05 13:02:21 +0100

    start day 4

---
+++ Solutions/days/Day4.cs
@@ -0,0 +1,19 @@
+namespace Days {
+    public class Day4
+    {
+        public static int SolvePartOne(string[] input)
+        {
+            return 0;
+        }
+        public static int SolvePartTwo(string[] input)
+        {
+            return 0;
+        }
+        public static void SolveDay4()
+        {
+            string[] lines = File.ReadAllLines("../inputs/input4.txt");
+            Console.WriteLine("Part one: ");
+            Console.WriteLine("Part two: ");
+        }
+    }
+}
---
+++ Test/days/TestDay4.cs
@@ -0,0 +1,17 @@
+using Days;
+namespace Test {
+    [TestClass]
+    public class TestDay4
+    {
+        [TestMethod]
+        public void TestPartOne()
+        {
+            // Test code here
+        }
+        [TestMethod]
+        public void TestPartTwo()
+        {
+            // Test code here
+        }
+    }
+}
