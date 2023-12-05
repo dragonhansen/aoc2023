b64f13feda4987bd9110b069752db83247654687
Oliver <oliv5023@gmail.com>
2023-12-05 15:22:55 +0100

    day4part1 done

--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -5,6 +5,7 @@ class Program
     {
         //Day1.SolveDayOne();
         //Day2.SolveDayTwo();
-        Day3.SolveDay3();
+        //Day3.SolveDay3();
+        Day4.SolveDay4();
     }
 }
--- Solutions/days/Day4.cs
+++ Solutions/days/Day4.cs
@@ -3,7 +3,29 @@ namespace Days {
     {
         public static int SolvePartOne(string[] input)
         {
-            return 0;
+            int sum = 0;
+            foreach(String line in input) {
+                string[] splitInput = line.Split(':')[1].Split('|');
+                string[] winnerNumbers = splitInput[0].Split(' ');
+                string[] myNumbers = splitInput[1].Split(' ');
+                int points = 1;
+                bool zeroPoints = true;
+                foreach(String myNumber in myNumbers) {
+                    // Have to make this check because the input is formatted in a dumb way
+                    if (myNumber.Equals("")) {
+                        continue;
+                    }
+                    if(winnerNumbers.Contains(myNumber)) {
+                        zeroPoints = false;
+                        points *= 2;
+                    }
+                }
+                if(zeroPoints) {
+                    continue;
+                }
+                sum += points/2;
+            }
+            return sum;
         }
         public static int SolvePartTwo(string[] input)
         {
@@ -12,7 +34,7 @@ namespace Days {
         public static void SolveDay4()
         {
             string[] lines = File.ReadAllLines("../inputs/input4.txt");
-            Console.WriteLine("Part one: ");
+            Console.WriteLine($"Part one: {SolvePartOne(lines)}");
             Console.WriteLine("Part two: ");
         }
     }
