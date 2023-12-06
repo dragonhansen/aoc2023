419b68464d8abef8846e579f82f74304c0f8f528
Oliver <oliv5023@gmail.com>
2023-12-06 14:46:37 +0100

    start day5

---
+++ Solutions/days/Day5.cs
@@ -0,0 +1,19 @@
+namespace Days {
+    public class Day5
+    {
+        public static int SolvePartOne(string[] input)
+        {
+            return 0;
+        }
+        public static int SolvePartTwo(string[] input)
+        {
+            return 0;
+        }
+        public static void SolveDay5()
+        {
+            string[] lines = File.ReadAllLines("../inputs/input5.txt");
+            Console.WriteLine("Part one: ");
+            Console.WriteLine("Part two: ");
+        }
+    }
+}
