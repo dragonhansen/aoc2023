6b1cae226356e82c96b54d150dd4a0c6d36d496c
Oliver <oliv5023@gmail.com>
2023-12-06 18:44:02 +0100

    start day6

---
+++ Solutions/days/Day6.cs
@@ -0,0 +1,19 @@
+namespace Days {
+    public class Day6
+    {
+        public static int SolvePartOne(string[] input)
+        {
+            return 0;
+        }
+        public static int SolvePartTwo(string[] input)
+        {
+            return 0;
+        }
+        public static void SolveDay6()
+        {
+            string[] lines = File.ReadAllLines("../inputs/input6.txt");
+            Console.WriteLine("Part one: ");
+            Console.WriteLine("Part two: ");
+        }
+    }
+}
